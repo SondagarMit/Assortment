23e2071f163e2ba364d39bda2524b0b2873ac816
Mitsondagar <sondagarmit@gmail.com>
2023-08-08 09:37:25 +0530

    Update Assignment

--- 2.C
+++ 2.C
@@ -1,7 +1,35 @@
 #include<stdio.h>
-#inlcude<conio.h>
+#include<conio.h>
 void main()
 {
+  int a[10],size,i,value,pos;
+  clrscr();
+   printf("Enter Your Value: ");
+   scanf("%d",&size);
+
+   for (i=0;i<size;i++)
+   {
+    scanf("%d",&a[i]);
 
 
-}
+   }
+    printf("Enter Your Ex. Value: ");
+    scanf("%d",&value);
+
+    printf("enter your Postion: ");
+    scanf("%d",&pos);
+
+    for (i=size;i>=pos;i--)
+    {
+       a[i+1]=a[i];
+    }
+    a[pos]=value;
+    size++;
+
+    for (i=0;i<size;i++)
+    {
+     printf("%d\n",a[i]);
+
+    }
+       getch();
+       }
